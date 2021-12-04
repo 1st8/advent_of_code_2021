0fc22452224208097fcb3e77abf187dd134d7ce7
Christoph Geschwind <christoph.geschwind@mixxt.de>
2021-12-04 16:20:05 +0100

    --- Day 3: Binary Diagnostic --- (formatting)

--- 03/part2.rb
+++ 03/part2.rb
@@ -5,9 +5,9 @@ lines = open(__dir__ + '/input').each_line.map { |l| l.strip! }
 def search(lines, search_most_common)
   last_matched = nil
   lines.first.length.times do |pos|
-    ones = lines.count { |l| l[pos] == "1" }
+    ones = lines.count { |l| l[pos] == '1' }
     zeroes = lines.count - ones
-    search = ones >= zeroes ? "1" : "0"
+    search = ones >= zeroes ? '1' : '0'
     lines.select! do |l|
       if search_most_common ? l[pos] == search : l[pos] != search
         last_matched = l
@@ -17,13 +17,11 @@ def search(lines, search_most_common)
   last_matched
 end
 
-oxygen_generator_rating =
-  search(lines.dup, true)
+oxygen_generator_rating = search(lines.dup, true)
 
 pp oxygen_generator_rating
 
-co2_scrubber_rating =
-  search(lines.dup, false)
+co2_scrubber_rating = search(lines.dup, false)
 
 pp co2_scrubber_rating
 
