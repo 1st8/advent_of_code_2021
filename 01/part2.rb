d75536d75c1476a456c99081dda9f6f4e2e82edb
Christoph Geschwind <christoph.geschwind@mixxt.de>
2021-12-09 20:57:32 +0100

    --- Day 1: Sonar Sweep --- (codegolfing part2)

--- 01/part2.rb
+++ 01/part2.rb
@@ -1,29 +1,13 @@
 #!/usr/bin/env ruby
 
-result = 0
-i = 0
-
-open(__dir__ + '/input')
-  .each_line
-  .lazy
-  .map(&:strip)
-  .map(&:to_i)
-  .reduce([Array.new(3), Array.new(3)]) do |acc, n|
-    curr, prev = acc
-
-    prev.shift
-    prev << curr.last
-
-    curr.shift
-    curr << n
-
-    i += 1
-
-    next [curr, prev] if i < 4
-
-    result += 1 if curr.sum > prev.sum
-
-    [curr, prev]
-  end
-
-puts result
+pp(
+  open(__dir__ + '/input')
+    .each_line
+    .lazy
+    .map(&:strip)
+    .map(&:to_i)
+    .each_cons(3)
+    .map(&:sum)
+    .each_cons(2)
+    .count { |prev, curr| curr > prev }
+)
